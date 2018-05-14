38210ddbf12850b4ae047089408b42d393b1fd05
Frank Ng <franknigi@hotmail.com>
2018-05-15 01:20:32 +0800

    phpver0.2

--- Homework2/myForm.php
+++ Homework2/myForm.php
@@ -16,13 +16,13 @@
 <?php
 
 $nameErr = $DoBErr = $UIDErr = $DPErr = $genderErr = $FTErr = "";
-$name = $DoB = $UID = $DP = $gender = $FT = "";
+$name = $DoB = $UID = $DP = $gender = $fav = "";
 $progTitle = $DPoutput = $fruitiac = $DoBoutput = "";
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($_POST["name"])){
-        $nameErr = "Name is required";
+        $nameErr = "*Name is required";
     }
     else{
         if(!preg_match("/^[a-zA-Z ]*$/", $_POST["name"])){
@@ -34,18 +34,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
 
+
     if(empty($_POST["DoB"])){
         $DoBErr = "*Date of birth is required";
     }
     else{
-        $DoBoutput = date("d F Y", strtotime($DoB));
+        $DoBoutput = date("d F Y", strtotime($_POST["DoB"]));
     }
 
     if(empty($_POST["UID"])){
         $UIDErr = "*UIC ID is required";
     }
     else{
-        $UID = outputTable($_POST["UID"]);
+        if(!preg_match("/^[a-z]{1}[0-9]{9}$/", $_POST["UID"])){
+            $UIDErr = "*UIC ID is required";
+        }
+        else {
+            $UID = outputTable($_POST["UID"]);
+        }
     }
 
 
@@ -60,38 +66,98 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $DPErr = "*DST Programme is required";
     }
     else{
-        $DP = outputTable($_POST["DP"]);
-        $fullname = prog($DP);
-        $DPoutput = $fullname . "(" . $DP . ")";
+        if(!preg_match("/^[A-Z]{3,4}$/", $_POST["DP"])){
+            $DPErr = "*DST Programme is required";
+        }
+        else {
+            $DP = outputTable($_POST["DP"]);
+            $fullname = prog($DP);
+            $DPoutput = $fullname . "(" . $DP . ")";
+        }
     }
 
-    if(empty($_POST["FT"])){
-        $FTErr = "*No favourites were selected";
-    }
-    else {
-        $FT += outputTable($_POST["FT"]);
-    }
-
+$mm = date("m", strtotime($_POST["DoB"]));
+$dd = date("d", strtotime($_POST["DoB"]));
 function fruitiac($mm, $dd) {
 //allocate friut depending on the date of birth
-    if($mm >= 1 && $mm <= 3){
-        if($mm == 1) {
-            if ($dd >= 14) {
+
+    switch($mm){
+        case 1: {
+            if($dd >= 14){
+                $fruitiac = "Cherry";
+                break;
+            }
+            if($dd <= 5){
+                $fruitiac = "Lychees";
+                break;
             }
         }
-        else if($mm == 2){
-
+        case 2: {$fruitiac = "Cherry";
+        break;
         }
-        else{
+        case 3: {
             if($dd <= 2){
-
+                $fruitiac = "Cherry";
+                break;
             }
+            elseif($dd >= 10){
+                $fruitiac = "Durian";
+                break;
+            }
+        }
+        case 4: {
+            if($dd <= 27){
+                $fruitiac = "Durian";
+                break;
+            }
+        }
+        case 6: {
+            if($dd >= 5 && $dd <= 29){
+                $fruitiac = "Starfruit";
+                break;
+            }
+        }
+        case 7: {
+            if($dd >= 11){
+                $fruitiac = "Orange";
+                break;
+            }
+        }
+        case 8: {
+            if($dd <= 27){
+                $fruitiac = "Orange";
+                break;
+            }
+        }
+        case 9: {
+            if($dd >= 17){
+                $fruitiac = "Banana";
+                break;
+            }
+        }
+        case 10: {
+            if($dd <= 21){
+                $fruitiac = "Lychees";
+                break;
+            }
+        }
+        case 11: {
+            if($dd >= 3){
+                $fruitiac = "Lychees";
+                break;
+            }
+        }
+        case 12: {
+            $fruitiac = "Lychees";
+            break;
+        }
+        default: {
+            $fruitiac = "You only like vegetables";
         }
     }
     return $fruitiac;
 
 }
-
 function prog($DP) {
 //use PHP switch statement
     $progTitle = "";
@@ -132,7 +198,7 @@ function outputTable($data){
     <br/>
     <br/>
 
-    Date of Birth: <input type="date" name="DoB" value="<?php echo date("d-m-Y", strtotime(DoB))?>">
+    Date of Birth: <input type="date" name="DoB" value="<?php echo date("d m Y", strtotime($_POST["DoB"]))?>">
 
     <br/>
     <br/>
@@ -157,14 +223,14 @@ function outputTable($data){
 
     Favourite things:<br/>
 
-        <input type="checkbox" value="<?php echo $FT?>">Watching Movies<br/>
-        <input type="checkbox" value="<?php echo $FT?>">Collecting Stamps<br/>
-        <input type="checkbox" value="<?php echo $FT?>">Eating Out<br/>
-        <input type="checkbox" value="<?php echo $FT?>">Badminton<br/>
-        <input type="checkbox" value="<?php echo $FT?>">Table Tennis<br/>
-        <input type="checkbox" value="<?php echo $FT?>">Playing the Guitar<br/>
-        <input type="checkbox" value="<?php echo $FT?>">Coffee with Friend<br/>
-        <input type="checkbox" value="<?php echo $FT?>">Playing on My Phone<br/>
+        <input type="checkbox" name="fav[]" value="Watching Movies">Watching Movies<br/>
+        <input type="checkbox" name="fav[]" value="Collecting Stamps">Collecting Stamps<br/>
+        <input type="checkbox" name="fav[]" value="Eating Out">Eating Out<br/>
+        <input type="checkbox" name="fav[]" value="Badminton">Badminton<br/>
+        <input type="checkbox" name="fav[]" value="Table Tennis">Table Tennis<br/>
+        <input type="checkbox" name="fav[]" value="Playing the Guitar">Playing the Guitar<br/>
+        <input type="checkbox" name="fav[]" value="Coffee with Friend">Coffee with Friend<br/>
+        <input type="checkbox" name="fav[]" value="Playing on My Phone">Playing on My Phone<br/>
 
     <br/>
 
@@ -183,13 +249,25 @@ function outputTable($data){
 
 <?php
 
-echo "<table border=1><tr><th>Name</th><td>" . $name . "</td></tr>
-    <tr><th>UIC ID</th><td>" . $UID . "</td></tr>
-    <tr><th>Gender</th><td>" . $gender . "</td></tr>
-    <tr><th>Programme</th><td>" . $DPoutput . "</td><tr/>
-    <tr><th>DOB</th><td>" . $DoBoutput . "</td></tr>
-    <tr><th>Fruitiac</th><td>" . $fruitiac . "</td></tr>
-    <tr><th>List of favourites</th><td>" . $FT . "</td></tr></table>";
+echo "<table border=1><tr><th>Name</th><td>" . $name . "</td></tr>";
+echo "<tr><th>UIC ID</th><td>" . $UID . "</td></tr>";
+echo "<tr><th>Gender</th><td>" . $gender . "</td></tr>";
+echo "<tr><th>Programme</th><td>" . $DPoutput . "</td><tr/>";
+echo "<tr><th>DOB</th><td>" . $DoBoutput . "</td></tr>";
+echo "<tr><th>Fruitiac</th><td>" . $fruitiac . "</td></tr>";
+echo "<tr><th>List of favourites</th><td>";
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["fav"])) {
+        $FTErr = "*No favourites were selected";
+    } else {
+        foreach ($_POST["fav"] as $value) {
+            echo "$value <br/>";
+        }
+    }
+}
+"</td></tr></table>";
+
+echo $fruitiac;
 
 ?>
 
